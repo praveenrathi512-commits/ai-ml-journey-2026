ffeda67acd07e3021cf22095c1cf9a8555fe3b02
praveenrathi512-commits <praveenrathi.512@gmail.com>
2026-01-18 23:15:30 +0530

    Day 2: Python if-else and decision making

---
+++ day2_conditions.py
@@ -0,0 +1,34 @@
+# Age Checker
+
+age = 23
+
+if age >= 18:
+  print("You're eligible to vote")
+else:
+  print("You're not eligible to vote")
+
+
+# Number Checker
+
+number = 7
+
+if number > 0:
+  print("Positive Number")
+elif number == 0:
+  print("Zero")
+else:
+  print("Negative Number")
+
+
+# Marks grading system
+
+marks = 90
+
+if marks >= 90:
+  print("Grade A")
+elif marks >= 75:
+  print("Grade B")
+elif marks >= 60:
+  print("Grade C")
+else:
+  print("Fail")
