f4077ade301f86f0176293236f8fe9540a410993
praveenrathi512-commits <praveenrathi.512@gmail.com>
2026-01-18 23:14:34 +0530

    Add files via upload

---
+++ day1_python_basics.py
@@ -0,0 +1,13 @@
+# Introduction
+
+name = "Praveen"
+age = 23
+
+print("My name is", name)
+print("My current age is", age)
+
+future_age = age + 5
+print("My age after 5 years will be", future_age)
+
+future_goal = "Becoming Maching Learning Engineer"
+print("My future goal is", future_goal)
