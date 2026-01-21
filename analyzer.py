4b4c5d28a3724108add3468ba3af0ea14d443305
Praveenrathi <praveenrathi.512@gmail.com>
2026-01-21 23:45:08 +0530

    Day 13: Refactored mini project with error handling & structure

--- analyzer.py
+++ analyzer.py
@@ -1,27 +1,40 @@
 import csv
 
+# Load CSV data into a list
 def load_data(filename):
   data = []
-  with open(filename,newline='') as file:
-    reader = csv.reader(file)
-    next(reader)
-    for row in reader:
-      data.append(row)
+  try:
+    with open(filename,newline='') as file:
+      reader = csv.reader(file)
+      next(reader)
+      for row in reader:
+        data.append(row)
+  except FileNotFoundError:
+    print(f'File {filename} not found')
   return data
 
+# Calculate statistics
 def calculate_average_age(data):
   total = 0 
   for row in data:
-    total += int(row[1])
+    try:
+     total += int(row[1])
+    except ValueError:
+      print("Invalid age value:", row)
   return total / len(data)
 
+# Filter based on conditions
 def filter_by_age(data, min_age):
   filtered = []
   for row in data:
-    if int(row[1]) > min_age:
-      filtered.append(row)
+    try:
+      if int(row[1]) > min_age:
+        filtered.append(row)
+    except ValueError:
+      pass
   return filtered
 
+# Save results to a report
 def write_report(filename, avg_age, filterd):
   with open(filename, "w") as file:
     file.write("DATA ANALYSIS REPORT\n")
@@ -32,10 +45,17 @@ def write_report(filename, avg_age, filterd):
       file.write(f"{row[0]} ({row[1]}) - {row[2]}\n")
 
 def main():
+
   data = load_data("people.csv")
+
+  if not data:
+    print("No data loaded.")
+    return
+  
   avg_age = calculate_average_age(data)
   filterd = filter_by_age(data, 25)
   write_report("report.txt", avg_age, filterd)
   print("Report generated sucessfully.")
 
-main()
+if __name__ == "__main__":
+  main()
